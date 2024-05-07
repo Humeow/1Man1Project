189d57e0f6689370c0ccece04bb9dd2c6788f7d7
Humeow <humeow0223@gmail.com>
2024-05-07 15:10:37 +0900

    temp

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ else:
 origins = ["http://localhost:8000",
            "http://127.0.0.1:8000",
            "http://158.179.173.108:2460/",
-           'http://wpwiki.humeow.xyz',
-           'https://wpwiki.humeow.xyz']
+           'http://wpwiki.humeow.kr',
+           'https://wpwiki.humeow.kr']
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
